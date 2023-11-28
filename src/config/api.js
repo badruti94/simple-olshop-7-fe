ea429616fea1c2c6df99d35dac144c8a425aa81f
Badru <badruakfm@gmail.com>
2023-11-29 04:08:12 +0700

    change baseurl, local to vercel

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const API = axios.create({
-    baseURL: 'http://localhost:4000/v1/'
+    // baseURL: 'http://localhost:4000/v1/'
+    baseURL: 'https://simple-olshop-7.vercel.app/v1/'
 })
 
 export const getConfig = async () => {
